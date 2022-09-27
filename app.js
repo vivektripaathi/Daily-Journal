b013f1ba2fe54491f3142303bc0893a699f987dc
Vivek Tripathi <vivektripathi8005@gmail.com>
2022-09-27 22:52:19 +0530

    Database Added

--- app.js
+++ app.js
@@ -2,22 +2,34 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash')
+const mongoose = require('mongoose');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "Head over to Compose menu that is inside the navbar. Give your post a beautiful title and then jot your beautiful blog";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-var posts = [];
+mongoose.connect('mongodb://127.0.0.1/blogPost', {useNewUrlParser: true});
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
 
 app.get("/", (req, res)=>{
-  res.render('home',{homeStartingContent:homeStartingContent,posts:posts});
+  Blog.find({}, (err, blogs) => {
+    res.render('home',{
+      homeStartingContent:homeStartingContent,
+      blogs:blogs
+    });
+  })
 });
 app.get("/about", (req, res)=>{
   res.render('about',{aboutContent:aboutContent});
@@ -29,18 +41,22 @@ app.get("/compose", (req, res)=>{
   res.render('compose');
 });
 app.post("/compose", (req, res)=>{
-  const post = {
-    postTitle : req.body.postTitle,
-    postBody : req.body.postBody
-  }
-  posts.push(post);
-  res.redirect("/");
-});
-app.get("/posts/:postName", (req, res)=>{
-  posts.forEach((post)=>{
-    if(_.kebabCase(post.postTitle) === _.kebabCase(req.params.postName)){
-      res.render('post', {title:post.postTitle, content:post.postBody})
+  const newBlog = new Blog({
+    title : req.body.postTitle,
+    content : req.body.postBody
+  });
+  newBlog.save((err) => {
+    if(!err){
+      res.redirect('/');
     }
+  });
+});
+app.get("/posts/:postId", (req, res)=>{
+  Blog.findOne({_id: req.params.postId}, (err, post) => {
+      res.render('post', {
+        title : post.title,
+        content : post.content
+      });
   });
 });
 
